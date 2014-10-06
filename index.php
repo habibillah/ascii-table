ef6c9c6810e21a0506efe9291822996959c7721e
Habibillah <habibillah@gmail.com>
2014-10-06 17:17:03 +0700

    add sample

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+use Utility\AsciiTable;
+
 require "vendor/autoload.php";
 
+header('Content-type: text/plain');
+
+$asciiTable = new AsciiTable(array(
+    array(
+        'Name' => 'Trixie',
+        'Color' => 'Green',
+        'Element' => 'Earth',
+        'Likes' => 'Flowers'
+    ),
+    array(
+        'Name' => 'Tinkerbell',
+        'Element' => 'Air',
+        'Likes' => 'Singning',
+        'Color' => 'Blue'
+    ),
+    array(
+        'Element' => 'Water',
+        'Likes' => 'Dancing',
+        'Name' => 'Blum',
+        'Color' => 'Pink'
+    ),
+));
+echo $asciiTable->render();
